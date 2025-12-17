ae49d6975cedd191450b356331f910207beda4cb
wa1d3r <yaremko2021@mail.ru>
2025-12-18 01:26:09 +0300

    create btns

--- Chess/src/graphic/sfml_graphics.h
+++ Chess/src/graphic/sfml_graphics.h
@@ -62,6 +62,114 @@ public:
         createBoardButtons();
     }
 
+    void createBoardButtons()
+    {
+        boardButtons.clear();
+        sf::Vector2u winSize = window.getSize();
+
+        // ... (код загрузки фона и расчета масштаба остается тем же) ...
+        const sf::Texture* bgTex = resourceManager.getTexture("background");
+        backgroundSprite = std::make_unique<sf::Sprite>(*bgTex);
+        float scaleX = static_cast<float>(winSize.x) / bgTex->getSize().x;
+        float scaleY = static_cast<float>(winSize.y) / bgTex->getSize().y;
+        backgroundSprite->setScale(sf::Vector2f(scaleX, scaleY));
+        backgroundSprite->setPosition(sf::Vector2f(0.f, 0.f));
+
+        float winW = static_cast<float>(winSize.x);
+        float winH = static_cast<float>(winSize.y);
+
+        // Параметры верстки
+        float marginRatio = 0.05f;
+        float clockWidthRatio = 0.25f;
+        float clockGapRatio = 0.05f;
+
+        float maxSideByHeight = winH * (1.0f - 2.0f * marginRatio);
+        float availableWidthForContent = winW * (1.0f - 2.0f * marginRatio);
+        float widthFactor = 1.0f + clockGapRatio + clockWidthRatio;
+        float maxSideByWidth = availableWidthForContent / widthFactor;
+        float boardSide = min(maxSideByHeight, maxSideByWidth);
+
+        float cellSizePx = boardSide / 8.0f;
+        float clockWidth = boardSide * clockWidthRatio;
+
+        // Высоты элементов правой панели
+        float clockHeight = boardSide * 0.15f;
+        float buttonHeight = boardSide * 0.10f;
+        float spacing = boardSide * 0.02f; // Отступы между элементами
+
+        // Остаток места под историю: Общая высота - 2 часов - кнопка - отступы
+        float historyHeight = boardSide - (2 * clockHeight) - buttonHeight - (3 * spacing);
+
+        // Координаты
+        float totalGroupWidth = boardSide + (boardSide * clockGapRatio) + clockWidth;
+        float startX = (winW - totalGroupWidth) / 2.0f;
+        float startY = (winH - boardSide) / 2.0f;
+
+        boardArea = sf::FloatRect(sf::Vector2f(startX, startY), sf::Vector2f(boardSide, boardSide));
+        float rightPanelX = startX + boardSide + (boardSide * clockGapRatio);
+
+        // 1. Верхние часы (Черные)
+        topClockArea = sf::FloatRect(sf::Vector2f(rightPanelX, startY), sf::Vector2f(clockWidth, clockHeight));
+
+        // 2. История ходов (Между часами)
+        float historyY = startY + clockHeight + spacing;
+        historyArea = sf::FloatRect(sf::Vector2f(rightPanelX, historyY), sf::Vector2f(clockWidth, historyHeight));
+
+        // 3. Нижние часы (Белые)
+        float bottomClockY = historyY + historyHeight + spacing;
+        bottomClockArea = sf::FloatRect(sf::Vector2f(rightPanelX, bottomClockY), sf::Vector2f(clockWidth, clockHeight));
+
+        // 4. Кнопка "Сдаться" (В самом низу)
+        float buttonY = bottomClockY + clockHeight + spacing;
+
+        sf::Vector2f btnRelPos(rightPanelX / winW, buttonY / winH);
+        sf::Vector2f btnRelSize(clockWidth / winW, buttonHeight / winH);
+
+        resignButton = std::make_unique<Button>(
+            btnRelPos, btnRelSize, winSize, 0.f,
+            [this]() { if (onResignCallback) onResignCallback(); },
+            "Resign", static_cast<unsigned int>(buttonHeight * 0.4f),
+            resourceManager.getFont("main_font"),
+            sf::Color(200, 100, 100), sf::Color(255, 100, 100), // Красные оттенки
+            sf::Color::White, sf::Color::Black);
+
+        // ... (код создания кнопок доски остается тем же) ...
+        auto texSize = resourceManager.getTexture("pawn_white")->getSize();
+        for (auto& buf : cellBuffers)
+        {
+            buf->resize(sf::Vector2u(texSize.x, texSize.y));
+        }
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                sf::Color color = sf::Color::Transparent;
+                sf::Color hover = sf::Color::Transparent;
+                Position buttonPosition = getChangedPos(Position(x, y));
+
+                float btnPxX = startX + buttonPosition.getX() * cellSizePx;
+                float btnPxY = startY + buttonPosition.getY() * cellSizePx;
+
+                sf::Vector2f relPos(btnPxX / winW, btnPxY / winH);
+                sf::Vector2f relSize(cellSizePx / winW, cellSizePx / winH);
+
+                auto btn = std::make_unique<Button>(
+                    relPos,
+                    relSize,
+                    winSize,
+                    0,
+                    [this, x, y]() {
+                        if (onSquareClickCallback)
+                            onSquareClickCallback(Position(x, y));
+                    },
+                    "", 0, nullptr,
+                    color, hover, sf::Color::Transparent, sf::Color::Transparent);
+
+                boardButtons.push_back(std::move(btn));
+            }
+        }
+    }
+
     void setOnClickCallback(std::function<void(Position)> callback)
     {
         onSquareClickCallback = callback;
