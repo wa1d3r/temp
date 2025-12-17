7ae147f5a99f7edef5b661739673f93cac07c6bb
wa1d3r <yaremko2021@mail.ru>
2025-12-18 00:58:50 +0300

    add resource mamager

---
+++ Chess/src/graphic/ResourceManager.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+
+class ResourceManager
+{
+
+public:
+    ResourceManager() = default;
+};
