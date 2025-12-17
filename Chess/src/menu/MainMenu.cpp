825d337469c98342e3fc5719f0fd5230629afa82
wa1d3r <yaremko2021@mail.ru>
2025-12-18 01:08:02 +0300

    add resize

--- Chess/src/menu/MainMenu.cpp
+++ Chess/src/menu/MainMenu.cpp
@@ -9,3 +9,12 @@ MainMenu::MainMenu(sf::RenderWindow& win, ResourceManager& rm)
     backgroundSprite = std::make_unique<sf::Sprite>(*resourceManager.getTexture("background"));
     resize();
 }
+
+void MainMenu::resize()
+{
+    const sf::Texture bgTex = backgroundSprite->getTexture();
+    float scaleX = static_cast<float>(window.getSize().x) / bgTex.getSize().x;
+    float scaleY = static_cast<float>(window.getSize().y) / bgTex.getSize().y;
+    backgroundSprite->setScale(sf::Vector2f(scaleX, scaleY));
+    initButtons();
+}
