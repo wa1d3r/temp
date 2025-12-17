826e9178747fbafb968371e1e6f448fa871f83cf
wa1d3r <yaremko2021@mail.ru>
2025-12-18 01:00:16 +0300

    work with textures

--- Chess/src/graphic/ResourceManager.h
+++ Chess/src/graphic/ResourceManager.h
@@ -7,7 +7,30 @@
 
 class ResourceManager
 {
+    std::map<std::string, sf::Texture> textures;
 
 public:
     ResourceManager() = default;
+
+    bool loadTexture(std::string name, std::string path)
+    {
+        sf::Texture tex;
+        if (tex.loadFromFile(path))
+        {
+            tex.setSmooth(true);
+            textures[name] = tex;
+            return true;
+        }
+        return false;
+    }
+
+    const sf::Texture* getTexture(std::string name) const
+    {
+        auto it = textures.find(name);
+        if (it != textures.end())
+        {
+            return it->second;
+        }
+        return nullptr;
+    }
 };
