8d7b9ce13c327905719dc955d410e6a3df4dade3
wa1d3r <yaremko2021@mail.ru>
2025-12-18 17:36:32 +0300

    add anim h

--- Chess/src/menu/MainMenu.h
+++ Chess/src/menu/MainMenu.h
@@ -7,6 +7,7 @@
 #include <functional>
 #include <memory>
 #include <vector>
+#include <ctime>
 
 enum class AppState
 {
@@ -36,7 +37,7 @@ struct GameConfig
     float timeMinutes = 10.0f;
     float incrementSeconds = 5.0f;
     int seed = 0;
-    std::string serverIp = "127.0.0.1"; // <-- Поле для IP
+    std::string serverIp = "127.0.0.1";
 };
 
 enum class MenuScreen
@@ -45,6 +46,15 @@ enum class MenuScreen
     GameSetup,
     Settings,
     About
+};
+
+enum class AnimType
+{
+    None,
+    KingChase,
+    PawnPromotion,
+    KnightsLeap,
+    BishopGlide
 };
 
 class MainMenu
@@ -57,7 +67,6 @@ class MainMenu
 
     std::vector<std::unique_ptr<Button>> mainButtons;
     std::vector<std::unique_ptr<Button>> setupButtons;
-    std::vector<std::unique_ptr<Button>> aboutButtons;
     std::vector<std::unique_ptr<Button>> backButton;
 
     std::vector<std::unique_ptr<InputBox>> inputBoxes;
@@ -65,22 +74,36 @@ class MainMenu
 
     std::unique_ptr<sf::Sprite> backgroundSprite;
 
-    sf::Text errorText; // <-- Текст ошибки
+    sf::Text errorText;
+    sf::Text aboutText;
 
     std::function<void(GameConfig)> onStartGame;
     std::function<void()> onExit;
+
+    // --- Анимация ---
+    sf::Clock dtClock;
+    float idleTimer;
+    AnimType currentAnim;
+
+    sf::Sprite animSprite1;
+    sf::Sprite animSprite2;
+
+    float animStateTime;
+    float animPhase;
+    bool animFlag;
+    bool secondActorActive; // <-- Новый флаг для задержки ладьи
 
 public:
     MainMenu(sf::RenderWindow& win, ResourceManager& rm);
 
     void handleEvent(const std::optional<sf::Event>& event);
+    void update();
     void draw();
     void resize();
 
     void setOnStartGame(std::function<void(GameConfig)> callback) { onStartGame = callback; }
     void setOnExit(std::function<void()> callback) { onExit = callback; }
 
-    // Метод для установки сообщения об ошибке
     void setErrorMessage(const std::string& message);
 
 private:
@@ -92,4 +115,10 @@ private:
     std::unique_ptr<Button> createBtn(
         sf::Vector2f pos, sf::Vector2f size, const std::string& text,
         std::function<void()> onClick, sf::Color color = sf::Color::White);
+
+    void startRandomAnimation();
+    void updateKingChase(float dt);
+    void updatePawnPromotion(float dt);
+    void updateKnightsLeap(float dt);
+    void updateBishopGlide(float dt);
 };
